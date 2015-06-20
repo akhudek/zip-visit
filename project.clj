5ce26530a26eeaf45b9de2600c9f08a69c133a7c
Dave Della Costa <ddellacosta@gmail.com>
2015-06-20 14:05:11 +0900

    bumps to Clojure 1.7, adds ClojureScript compatibility via reader conditionals

--- project.clj
+++ project.clj
@@ -1,10 +1,14 @@
-(defproject zip-visit "1.0.2"
+(defproject zip-visit "1.1.0-SNAPSHOT"
   :description "Clojure zipper-based visitor library."
   :url "https://github.com/akhudek/zip-visit"
   :license {:name "MIT"
             :url  "http://opensource.org/licenses/MIT"}
   :dependencies []
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}}
+  :profiles {:provided
+             {:dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                             [org.clojure/clojurescript "0.0-3308"]]}
+             :dev
+             {:dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
   :scm {:name "git"
         :url  "https://github.com/akhudek/zip-visit"}
   :deploy-repositories
