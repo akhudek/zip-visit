9a268be7868aa16eafaf6080a09d5a6f8af9be17
Alexander K. Hudek <alexander@hudek.org>
2015-07-07 01:53:46 -0400

    Bump to final version of clojure 1.7.0 and give non-snapshot number.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject zip-visit "1.1.0-SNAPSHOT"
+(defproject zip-visit "1.1.0"
   :description "Clojure zipper-based visitor library."
   :url "https://github.com/akhudek/zip-visit"
   :license {:name "MIT"
             :url  "http://opensource.org/licenses/MIT"}
   :dependencies []
   :profiles {:provided
-             {:dependencies [[org.clojure/clojure "1.7.0-RC2"]
+             {:dependencies [[org.clojure/clojure "1.7.0"]
                              [org.clojure/clojurescript "0.0-3308"]]}
              :dev
              {:dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
